931a8488055899da9cf36326dc871a7753806ba2
Fadi-B <s1969801@ed.ac.uk>
2020-04-23 21:06:34 +0200

    Init

---
+++ src/main/java/se/fadi/arcanusverse/Arcanusverse.java
@@ -0,0 +1,17 @@
+package se.fadi.arcanusverse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Arcanusverse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
